e11d1ef4da60eb4b128809630cd8ee15249b83a3
Hunain <hunainkhan142001@gmail.com>
2023-02-13 12:56:57 +0530

    seventh commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='my_project',
+    version='0.1',
+    author='Hunain',
+    author_email='hunainkhan142001@gmail.com',
+    description='A description of your project',
+    install_requires=['tkinter'],
+    packages=['my_project'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
